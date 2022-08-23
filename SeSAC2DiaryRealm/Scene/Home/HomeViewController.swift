2f49fccf496abe21772a415669484ff67d9df194
ByeongHyeon <qudgus1984@naver.com>
2022-08-23 11:54:59 +0900

    [SeSAC] Week8 Day 2 즐겨찾기 버튼 추가

--- SeSAC2DiaryRealm/Scene/Home/HomeViewController.swift
+++ SeSAC2DiaryRealm/Scene/Home/HomeViewController.swift
@@ -9,27 +9,34 @@ import UIKit
 import SnapKit
 import RealmSwift //Realm 1. import
 
+
+
 class HomeViewController: UIViewController {
     
     let localRealm = try! Realm() // Realm 2.
 
-    let tableView: UITableView = {
+    lazy var tableView: UITableView = {
         let view = UITableView()
-        view.backgroundColor = .lightGray
+        view.rowHeight = 60
+        view.delegate = self
+        view.dataSource = self
         return view
-    }()
+    }() //즉시 실행 클로저
     
-    var tasks: Results<UserDiary>!
+    var tasks: Results<UserDiary>! {
+        didSet {
+            tableView.reloadData()
+            print("Tasks Changed")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //Realm 3. Realm 데이터를 정렬해 tasks 에 담기
-        let tasks = localRealm.objects(UserDiary.self).sorted(byKeyPath: "diaryTitle", ascending: false)
+        let tasks = localRealm.objects(UserDiary.self).sorted(byKeyPath: "diaryTitle", ascending: true)
         print(tasks)
         
-        tableView.delegate = self
-        tableView.dataSource = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         view.addSubview(tableView)
         tableView.snp.makeConstraints { make in
@@ -37,15 +44,25 @@ class HomeViewController: UIViewController {
         }
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(plusButtonClicked))
+        
+        let sortButton = UIBarButtonItem(title: "정렬", style: .plain, target: self, action: #selector(sortButtonClicked))
+        let filterButton = UIBarButtonItem(title: "필터", style: .plain, target: self, action: #selector(filterButtonClicked))
+        navigationItem.leftBarButtonItems = [sortButton, filterButton]
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print(#function)
+        fetchRealm()
         //화면 갱신은 화면 전환 코드 및 생명 주기 실행 점검 필요!
         //present, overCurrentContext, overFullScreen > viewWillApear X
-        tasks = localRealm.objects(UserDiary.self).sorted(byKeyPath: "diaryDate", ascending: false)
-        tableView.reloadData()
+        tasks = localRealm.objects(UserDiary.self).sorted(byKeyPath: "diaryTitle", ascending: true)
+        
+    }
+    
+    func fetchRealm() {
+        //Realm 3. Realm 데이터를 정렬해 tasks 에 담기
+        let tasks = localRealm.objects(UserDiary.self).sorted(byKeyPath: "diaryTitle", ascending: true)
     }
     
     @objc func plusButtonClicked() {
@@ -53,6 +70,17 @@ class HomeViewController: UIViewController {
         vc.modalPresentationStyle = .fullScreen
         present(vc, animated: true)
     }
+    
+    @objc func sortButtonClicked() {
+        tasks = localRealm.objects(UserDiary.self).sorted(byKeyPath: "diaryTitle", ascending: true)
+    }
+    
+    //realm filter query, NSPredicate
+    @objc func filterButtonClicked() {
+        tasks = localRealm.objects(UserDiary.self).filter("diaryTitle CONTAINS[c] '일기'") //대소문자 여부
+            //.filter("diaryTitle = '오늘의 일기36'")
+    }
+    
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
@@ -65,4 +93,41 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         cell.textLabel?.text = tasks[indexPath.row].diaryTitle
         return cell
     }
+    
+    //참고. TableView Editing Mode
+    //테이블뷰 셀 높이가 작을 경우, 이미지가 없을 때, 시스템 이미지가 아닌 경우
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let favorite = UIContextualAction(style: .normal, title: "즐겨찾기") { action, view, completionHandler in
+            
+            //realm data update
+            try! self.localRealm.write {
+                //하나의 레코드에서 특정 컬럼 하나만 변경
+                self.tasks[indexPath.row].favorite = !self.tasks[indexPath.row].favorite
+                
+                //하나의 테이블에 특정 컬럼 전체 값을 변경
+                //self.tasks.setValue(true, forKey: "favorite")
+                
+                //하나의 레코드에서 여러 칼럼들이 변경
+                //self.localRealm.create(UserDiary.self, value: ["objectId": self.tasks[indexPath.row].objectId, "diaryContent": "변경 테스트", "diaryTitle": "제목임"], update: .modified)
+                
+                print("Realm Update Succeed")
+            }
+            // 1.스와이프 셀 하나만 ReloadRows 코드를 구현 => 상대적 효율성
+            // 2.데이터가 변경되었으니 다시 realm에서 데이터를 가지고오기 => didSet 일관적 형태로 갱신
+//            self.fetchRealm()
+            tableView.reloadData()
+        }
+        
+        //realm 데이터 기준
+        let image = tasks[indexPath.row].favorite ? "star.fill" : "star"
+        favorite.image = UIImage(systemName: image)
+        favorite.backgroundColor = .systemPink
+        
+
+        
+        return UISwipeActionsConfiguration(actions: [favorite])
+    }
+    
+
 }
